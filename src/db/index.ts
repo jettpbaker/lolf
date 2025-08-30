5bcbd8606c0ee907beb20f9000cb53523e876179
Jett Baker <jett.p.baker@gmail.com>
2025-08-30 12:13:43 +1000

    fixed env name

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 const databaseUrl =
   process.env.DRIZZLE_TARGET === 'prod'
-    ? process.env.DATABASE_URL
+    ? process.env.PROD_DATABASE_URL
     : process.env.DEV_DATABASE_URL;
 
 // biome-ignore lint/style/noNonNullAssertion: env exists
